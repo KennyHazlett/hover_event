f78fe5dbbbcf717c02c74fdbef3029c2450b0d01
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-07-05 09:17:25 -0400

    first commit

---
+++ mouseout.js
@@ -0,0 +1,18 @@
+function over(element) {
+    element.style.backgroundColor = "lime";    
+}
+    
+function out(element) {
+    element.style.backgroundColor = "silver";   
+}
+function over(element) {
+    alert("mouseover");    
+}
+    
+function out(element) {
+    alert("mouseout");    
+}
+
+
+
+
